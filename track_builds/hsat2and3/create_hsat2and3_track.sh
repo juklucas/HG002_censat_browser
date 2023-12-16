daa170ba531624ba4252a2183100f64dcde4411b
juklucas <juklucas@ucsc.edu>
2023-12-16 12:23:38 -0800

    add conda env

--- track_builds/hsat2and3/create_hsat2and3_track.sh
+++ track_builds/hsat2and3/create_hsat2and3_track.sh
@@ -8,6 +8,7 @@ gsutil cp \
     'gs://fc-7dceaa86-474e-4663-8830-62abdbd11463/submissions/85498b59-893d-4556-af50-051d33f2a0eb/identify_hSat2and3_wf/f4b9d01f-95b1-44b9-bea9-a5e963fb9b44/call-identify_hSat2and3/attempt-2/glob-0cf2dda8b61bdd48f400d7bffd4c5d48/hg002v1.0.HSat2and3_Regions.bed' submissions/85498b59-893d-4556-af50-051d33f2a0eb/identify_hSat2and3_wf/f4b9d01f-95b1-44b9-bea9-a5e963fb9b44/call-identify_hSat2and3/attempt-2/glob-0cf2dda8b61bdd48f400d7bffd4c5d48/hg002v1.0.HSat2and3_Regions.bed \
     ./
 
+conda activate ucsc-bedtobigbed
 
 bedToBigBed \
     -extraIndex=name \
@@ -19,6 +20,7 @@ bedToBigBed \
     hg002v1.0.fasta.2bit \
     hg002v1.0.HSat2and3_Regions.bb 
 
+conda deactivate ucsc-bedtobigbed
 
 ## Don't forget to update the trackDb.txt
 
