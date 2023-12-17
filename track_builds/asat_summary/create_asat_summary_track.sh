56546f0207c5071304d482776cda079239e884f1
juklucas <juklucas@ucsc.edu>
2023-12-17 15:43:20 -0800

    minor fix

--- track_builds/asat_summary/create_asat_summary_track.sh
+++ track_builds/asat_summary/create_asat_summary_track.sh
@@ -12,7 +12,7 @@ bedToBigBed \
     -extraIndex=name \
     -type=bed9 \
     -tab \
-    -as=.HG002_censat_browser/track_builds/asat_summary/asat_summary.as \
+    -as=HG002_censat_browser/track_builds/asat_summary/asat_summary.as \
     -sizesIs2Bit \
     HG002_asat_summary.bed \
     hg002v1.0.fasta.2bit \
